87d2da10f144b1259d121a9a42c039986bebea36
David Daeschler <david.daeschler@gmail.com>
2015-08-03 20:21:47 -0500

    Begin working on default rooms.

--- pchat/config.js
+++ pchat/config.js
@@ -23,4 +23,10 @@ config.uploadDir = './upload/';
 config.maxUploadSize = 4194304;
 config.imageRetentionPeriod = 172800000; //48 hrs in ms
 
+/**
+ * Default chat rooms can be specified here that all new users
+ * will be placed into when they first register
+ */
+config.defaultChatRooms = [];
+
 module.exports = config;
